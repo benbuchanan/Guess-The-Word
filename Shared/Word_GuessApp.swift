03b8cc6c8e8e627319f9ff18140524b005a1e62a
benbuchanan <benbuchanan11@yahoo.com>
2022-01-30 16:20:29 -0500

    Initial Commit

---
+++ Shared/Word_GuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_GuessApp.swift
+//  Shared
+//
+//  Created by Ben Buchanan on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Word_GuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
